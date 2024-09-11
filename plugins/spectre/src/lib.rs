e86a858836e224b72f5db7acb012769cd5274a84
rohanku <rohankumar@berkeley.edu>
2024-09-10 17:05:10 -0700

    Revert "Fix Spectre transient step size parameter (#39)"
    This reverts commit 4a1ea6dd1a1c1f16664222cab29c8c768947986b.

--- plugins/spectre/src/lib.rs
+++ plugins/spectre/src/lib.rs
@@ -418,7 +418,7 @@ fn analysis_line(input: &Analysis, prefix: &str, num: usize) -> Result<String> {
                 String::new()
             };
             let mut line = format!(
-                "{name} tran maxstep={} stop={} start={}{}",
+                "{name} tran step={} stop={} start={}{}",
                 a.step, a.stop, a.start, strobe
             );
             for (k, v) in a.opts.iter() {
